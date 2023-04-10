fa43b11b11c42a3b6185d0c349f069b6e51830be
skydiver8181 <skydiver8181@gmail.com>
2023-04-10 19:12:28 +0200

    10.04.2023

--- login_test.py
+++ login_test.py
@@ -1,32 +1,34 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-try:
-    link = "https://v8ru.ru/"
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    browser.get(link)
+def login():
+    try:
+        link = "https://v8ru.ru/"
+        browser = webdriver.Chrome()
+        browser.maximize_window()
+        browser.get(link)
 
-    login = browser.find_element(By.ID, "logInModal")
-    login.click()
+        login = browser.find_element(By.ID, "logInModal")
+        login.click()
 
-    enter_login = browser.find_element(By.ID, "login_modal")
-    enter_login.send_keys("ivan273681@gmail.com")
+        enter_login = browser.find_element(By.ID, "login_modal")
+        enter_login.send_keys("ivan273681@gmail.com")
 
-    enter_password = browser.find_element(By.ID, "pass_modal")
-    enter_password.send_keys("123456")
+        enter_password = browser.find_element(By.ID, "pass_modal")
+        enter_password.send_keys("123456")
 
-    button_enter = browser.find_element(By.ID, "go_modal")
-    button_enter.click()
+        button_enter = browser.find_element(By.ID, "go_modal")
+        button_enter.click()
 
-    user_name_value = browser.find_element(By.CSS_SELECTOR, "[class='clientName']")
-    user_name = user_name_value.get_attribute("value")
-    if user_name != "Вход":
-        print("Login function works correct")
-    else: print("Login function works incorrect")
+        user_name_value = browser.find_element(By.CSS_SELECTOR, "[class='clientName']")
+        user_name = user_name_value.get_attribute("value")
 
-finally:
-    time.sleep(3)
-    browser.quit()
+        assert user_name != "Вход", "Login function works incorrect"
+
+
+    finally:
+        browser.quit()
+
+def test_login():
+    login()
 
---
+++ suit_1_test.py
@@ -0,0 +1,7 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestSuit_1(unittest.TestCase)
+    def
